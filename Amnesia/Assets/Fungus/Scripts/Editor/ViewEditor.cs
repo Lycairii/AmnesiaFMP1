ed551464233e4e89ff0dde010788fb9a0f78407b
Lycairii <gem.creed@hotmail.co.uk>
2024-02-19 10:24:33 +0000

    Had to fix version of unity
    Fixed version of unity.
    Kept crashing in anything below 2023 and wouldn't load assets/ packages.

--- Amnesia/Assets/Fungus/Scripts/Editor/ViewEditor.cs
+++ Amnesia/Assets/Fungus/Scripts/Editor/ViewEditor.cs
@@ -137,7 +137,7 @@ namespace Fungus.EditorUtils
 
             for (int i = 0; i < 2; ++i)
             {
-                var fmh_141_57_638429971421563455 = Quaternion.identity; Vector3 newPos = Handles.FreeMoveHandle(handles[i],
+                var fmh_141_57_638439339318178995 = Quaternion.identity; Vector3 newPos = Handles.FreeMoveHandle(handles[i],
                                                         HandleUtility.GetHandleSize(pos) * 0.1f,
                                                         Vector3.zero,
 #if UNITY_5_6_OR_NEWER
